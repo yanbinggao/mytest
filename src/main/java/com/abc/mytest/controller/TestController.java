7ccd6f6c8f9307933692be2fccb4ac543d726a7e
yanbinggao <gaoyanbingbj@126.com>
2022-04-24 11:59:23 +0800

    v6.0.0

--- src/main/java/com/abc/mytest/controller/TestController.java
+++ src/main/java/com/abc/mytest/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "Hello Jenkins --- v6.0.0";
+        return "Hello Jenkins --- v6.0.1";
     }
 }
